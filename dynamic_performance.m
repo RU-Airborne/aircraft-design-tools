b58474863c2baf97648d2ecbdc1e7b84fb91afe2
nuomegaalpha <59454442+nuomegaalpha@users.noreply.github.com>
2023-08-03 00:21:59 -0400

    Update dynamic_performance.m

--- dynamic_performance.m
+++ dynamic_performance.m
@@ -17,7 +17,7 @@ V = linspace(0, 80, N_points);
 rho = 0.002377; % slugs / ft^3
 q = 1/2 .* rho .* V.^2;
 
-CD0 = 0.02794;
+CD0 = 0.03361;
 S = 2.44; % ft^2
 b = 44 / 12; % ft
 
@@ -28,23 +28,30 @@ K = 1 / (pi * oswald_eff * AR);
 
 % calculate drag
 D = q .* S .* (CD0 + K .* CL_max .^2);
+P_req = D .* V;
+
 
 V_8000 = table2array(data_9X5_8000(:,"V_prop"));
 T_8000 = table2array(data_9X5_8000(:,"T_LBF"));
+P_8000 = T_8000 .* V_8000;
 
 V_9000 = table2array(data_9X5_9000(:,"V_prop"));
 T_9000 = table2array(data_9X5_9000(:,"T_LBF"));
+P_9000 = T_9000 .* V_9000;
 
 V_10000 = table2array(data_9X5_10000(:,"V_prop"));
 T_10000 = table2array(data_9X5_10000(:,"T_LBF"));
+P_10000 = T_10000 .* V_10000;
 
 V_11000 = table2array(data_9X5_11000(:,"V_prop"));
 T_11000 = table2array(data_9X5_11000(:,"T_LBF"));
+P_11000 = T_11000 .* V_11000;
 
 V_12000 = table2array(data_9X5_12000(:,"V_prop"));
 T_12000 = table2array(data_9X5_12000(:,"T_LBF"));
+P_12000 = T_12000 .* V_12000;
 
-f = figure();
+f = figure("Name","Thrust");
 
 plot(V, D, "DisplayName", "Drag")
 hold on
@@ -65,3 +72,20 @@ latex_graph(f);
 % bonus:
 % power required vs. velocity
 % power available vs. velocity
+f = figure("Name","Power");
+
+plot(V, P_req, "DisplayName", "Power Required")
+hold on
+plot(V_8000, P_8000,   "--", "DisplayName","Power, RPM = 8000")
+plot(V_9000, P_9000,   "--",    "DisplayName","Power, RPM = 9000")
+plot(V_10000, P_10000,   "--",  "DisplayName","Power, RPM = 10000")
+plot(V_11000, P_11000,   "--",  "DisplayName","Power, RPM = 11000")
+plot(V_12000, P_12000,   "--",  "DisplayName","Power, RPM = 12000")
+
+legend
+xlabel("Velocity (ft/s)");
+ylabel("Power (lbf ft / s)");
+title("APC 9$\times$5 Prop")
+axis([0, 80, 0, 200])
+latex_graph(f);
+
